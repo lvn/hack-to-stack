87eba10ef81e212f7f61511b5700f13252b62e70
Elvin Yung <elvinyung1@gmail.com>
2014-09-22 23:00:23 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# hack-to-stack
+### by [Elvin Yung](https://github.com/elvinyung)
+
+## Description
+Turns all instances of `hack` into `stack` in Chrome. You asked for this.
+
+Vaguely stolen from [cloud-to-butt](https://github.com/panicsteve/cloud-to-butt).
+
+## Instructions
+Install from [Chrome App Store]()!
+
+Or just get the crx file from this repo and install it.
---
+++ app/content_script.js
@@ -0,0 +1,39 @@
+var main = function() {
+  walk(document.body);
+
+  function walk(node) 
+  {
+    // stolen from http://is.gd/mwZp7E
+    
+    var child, next;
+
+    switch ( node.nodeType )  
+    {
+      case 1:  // Element
+      case 9:  // Document
+      case 11: // Document fragment
+        child = node.firstChild;
+        while ( child ) 
+        {
+          next = child.nextSibling;
+          walk(child);
+          child = next;
+        }
+        break;
+      case 3: // Text node
+        if(node.parentElement.tagName.toLowerCase() != "script") {
+            handleText(node);
+        }
+        break;
+    }
+  }
+
+  function handleText(textNode) {
+		var v = textNode.nodeValue;
+		v = v.replace(/hack/g, "stack");
+		v = v.replace(/Hack/g, "Stack");
+		textNode.nodeValue = v;
+  }
+};
+
+main();
